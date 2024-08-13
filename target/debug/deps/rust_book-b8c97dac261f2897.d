4638fe82443a80f66f6b69e2535eeb81ad117f77
min-nguyen <minhnguyen1995@gmail.com>
2024-08-13 18:15:27 +0100

    revised _3_references.rs

--- target/debug/deps/rust_book-b8c97dac261f2897.d
+++
@@ -1,5 +0,0 @@
-/home/min/Documents/rust-stuff/rust-book/target/debug/deps/librust_book-b8c97dac261f2897.rmeta: src/main.rs
-
-/home/min/Documents/rust-stuff/rust-book/target/debug/deps/rust_book-b8c97dac261f2897.d: src/main.rs
-
-src/main.rs:

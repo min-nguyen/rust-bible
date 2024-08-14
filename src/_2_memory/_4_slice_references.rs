b0b180686ac1e5db693afdf301fb832d21c1d2dd
min-nguyen <minhnguyen1995@gmail.com>
2024-08-14 12:38:16 +0100

    _4_slice_reference notes

--- src/_2_memory/_4_slice_references.rs
+++ src/_2_memory/_4_slice_references.rs
@@ -1,100 +1,117 @@
 // -----------------------------------------------
 // # SLICE REFERENCES
-// A SLICE [T] is a contiguous sequence of data of type T stored in memory, being a slice of some collection rather than the entire collection itself.
-// A SLICE REFERENCE &[T] (or fat pointer) is represented in the stack as two parts:
-//    1. a pointer to the heap memory holding the contents of the slice
+//
+// A slice [T] is a contiguous sequence of data of type T stored in memory, being a slice of some collection rather than the entire collection itself.
+//
+// A slice reference &[T] (or fat pointer) is represented in the stack as two parts:
+//       { ptr: ..., len: ... }
+//    1. a pointer to the memory holding the contents of the slice
 //    2. a slice length
-// By including the slice length in the reference, this lets us talk about part of a data and give our reference a known size to refer to, while allowing the size of the slice reference type itself to be known.
+// By including the slice length in the reference value, this lets us:
+//    * Know the size of the slice at run-time that we can safely refer to.
+//    * Know the size of the slice reference type &[T] at compile-time.
 
-// A SLICE REFERENCE to STACK-ALLOCATED data:
 fn arrslice_example() {
-  let arr: [i32; 5] = [1, 2, 3, 4, 5]; // An array stored on the stack
+  // arr is an array on the stack
+  let arr: [i32; 5] = [1, 2, 3, 4, 5];
+
+  // arr_ref is a normal reference to arr
+  // its type includes the array's length, determined at compile-time.
   let arr_ref: &[i32; 5] = &arr;       // A normal reference to all of the array.
-                                       // Its type includes the array's length, determined at compile-time.
-  let slice: &[i32] = &arr[1..4];      // A slice reference to part of the array (elements 2, 3, and 4),
-                                       // Its value includes the slice's length, determined at run time.
-  let wholeslice: &[i32] = &arr[..];   // A slice reference to all of the array
-                                       // Its value includes the slice's length, determined at run time.
-}
-//  STACK:
-// +------------------------------------+
-// | Stack Frame: slice_example         |
-// +------------------------------------+ 0x7ffeefbff4a0
-// | arr: [1, 2, 3, 4, 5]               |  <--- `arr` is an array stored on the stack
-// |    (address = 0x7ffeefbff4a0,      |
-// |             ..,                    |
-// |             ..,                    |
-// |             ..,                    |
-// |             ..0x7ffeefbff4b3 )     |
-// +------------------------------------+ 0x7ffeefbff4b4
-// | arr_ref:  0x7ffeefbff4a0           |  <--- `arr_ref` is a reference to the array.
-// |                                    |  The length of the array is known at compile-time, and need not be stored in memory.
-// +------------------------------------+ 0x7ffeefbff4bc
-// | slice: { ptr: 0x7ffeefbff4a4,      |  <--- `slice` is a slice reference to an array portion starting `arr[1]` to `arr[3]`.
-// |          len: 3 }                  |
-// +------------------------------------+ 0x7ffeefbff4c4
-// | wholeslice: { ptr: 0x7ffeefbff4a0, |  <--- `wholeslice` is a slice reference to the entire array portion starting `arr[0]` to `arr[4]`.
-// |               len: 5 }             |
-// +------------------------------------+
 
-// A SLICE REFERENCE to HEAP-ALLOCATED data:
+  // arr_sliceref is a slice reference from elements 2 to 4.
+  // its value includes the slice's length, determined at run time.
+  let arr_sliceref: &[i32] = &arr[1..4];
+
+  // arr_wholesliceref is a slice reference to all of the array
+  // its value includes the slice's length, determined at run time.
+  let arr_wholesliceref: &[i32] = &arr[..];
+}
+    // Informal Mental Model: what *COULD* happen:
+    //  STACK:
+    // +------------------------------------+
+    // | Stack Frame: slice_example         |
+    // +------------------------------------+ 0x7ffeefbff4a0
+    // | arr: [1, 2, 3, 4, 5]               |  <--- `arr` is an array stored on the stack
+    // |    (address = 0x7ffeefbff4a0,      |
+    // |             ..,                    |
+    // |             ..,                    |
+    // |             ..,                    |
+    // |             ..0x7ffeefbff4b3 )     |
+    // +------------------------------------+ 0x7ffeefbff4b4
+    // | arr_ref:  0x7ffeefbff4a0           |  <--- `arr_ref` is a reference to the array.
+    // |                                    |  The length of the array is known at compile-time, and need not be stored in memory.
+    // +------------------------------------+ 0x7ffeefbff4bc
+    // | arr_sliceref:                      |  <--- `slice` is a slice reference to an array portion starting `arr[1]` to `arr[3]`.
+    // |  { ptr: 0x7ffeefbff4a4, len: 3 }   |
+    // +------------------------------------+ 0x7ffeefbff4c4
+    // | wholeslice:                        |  <--- `wholeslice` is a slice reference to the entire array portion starting `arr[0]` to `arr[4]`.
+    // |  { ptr: 0x7ffeefbff4a0, len: 5 }   |
+    // +------------------------------------+
+
 fn vecslice_example() {
-  let vec: Vec<i32> = vec![1, 2, 3, 4, 5]; // An vector stored on the heap
-  let vec_ref: &Vec<i32>  = &vec;          // A normal reference to all of the vector.
-                                           // Its value includes the vector's length, determined at run-time.
-  let slice: &[i32] = &vec[1..4];          // A slice reference to part of the vector (elements 2, 3, and 4),
-                                           // Its value includes the slice's length, determined at run time.
-  let wholeslice: &[i32] = &vec[..];       // A slice reference to all of the vector
-                                           // Its value includes the slice's length, determined at run time.
-}
-// A slice reference to heap-allocated data is similar to above for stack-allocated data, except:
-//    vec is now a value of the shape { ptr: ..., len: 5, capacity: .... } that points to an vector on the heap.
-//    vec_ref, slice, and wholeslice, follow the same format as before in the stack-allocated example.
+  // vec manages a vector on the heap
+  let vec: Vec<i32> = vec![1, 2, 3, 4, 5];
 
-//  STACK:
-// +------------------------------------------+
-// | Stack Frame: vecslice_example            |
-// +------------------------------------------+ 0x7ffeefbff498   <--- owner of heap-allocated vector
-// | vec: Vec {                               |
-// |   ptr: 0x60001234,                       | 8 bytes (pointer to vec[0] on heap)
-// |   len: 5,                                | 8 bytes
-// |   capacity: ..                           | 8 bytes
-// | }                                        |
-// +------------------------------------------+ 0x7ffeefbff4b0  <--- reference to the vector.
-// | vec_ref: &Vec {                          |
-// |   ptr: 0x7ffeefbff498                    | 8 bytes (pointer to `vec` on stack)
-// | }                                        |
-// +------------------------------------------+ 0x7ffeefbff4b8  <--- reference to the vector slice `vec[1]` to `vec[3]`.
-// | slice: {                                 |
-// |   ptr: 0x60001238,                       | 8 bytes (pointer to `vec[1]` on heap)
-// |   len: 3                                 | 8 bytes
-// | }                                        |
-// +------------------------------------------+ 0x7ffeefbff4c8 <--- reference to entire vector as a slice `vec[0]` to `vec[4]
-// | wholeslice: {                            |
-// |   ptr: 0x60001234,                       | 8 bytes (pointer to `vec[0]` on heap)
-// |   len: 5                                 | 8 bytes
-// | }                                        |
-// +------------------------------------------+ 0x7ffeefbff4d8
-//  HEAP:
-// +------------------------------------------+ 0x60001234
-// | 1                                        | <--- vec[0]
-// +------------------------------------------+ 0x60001238
-// | 2                                        | <--- vec[1]
-// +------------------------------------------+ 0x6000123C
-// | 3                                        | <--- vec[2]
-// +------------------------------------------+ 0x60001240
-// | 4                                        | <--- vec[3]
-// +------------------------------------------+ 0x60001244
-// | 5                                        | <--- vec[4]
-// +------------------------------------------+
+  // arr_ref is a normal reference to vec
+  // its value includes the vector's length, determined at run-time.
+  let vec_ref: &Vec<i32>  = &vec;
+
+  // vec_sliceref is a slice reference from elements 2 to 4.
+  // its value includes the slice's length, determined at run time.
+  let vec_sliceref: &[i32] = &vec[1..4];
+
+  // vec_wholesliceref is a slice reference to all of the vector
+  // its value includes the slice's length, determined at run time.
+  let vec_wholesliceref: &[i32] = &vec[..];
+}
+    // Informal Mental Model: what *COULD* happen:
+    //  STACK:
+    // +------------------------------------------+
+    // | Stack Frame: vecslice_example            |
+    // +------------------------------------------+ 0x7ffeefbff498   <--- owner of heap-allocated vector
+    // | vec: Vec {                               |
+    // |   ptr: 0x60001234,                       | 8 bytes (pointer to vec[0] on heap)
+    // |   len: 5,                                | 8 bytes
+    // |   capacity: ..                           | 8 bytes
+    // | }                                        |
+    // +------------------------------------------+ 0x7ffeefbff4b0  <--- reference to the vector.
+    // | vec_ref: &Vec {                          |
+    // |   ptr: 0x7ffeefbff498                    | 8 bytes (pointer to `vec` on stack)
+    // | }                                        |
+    // +------------------------------------------+ 0x7ffeefbff4b8  <--- reference to the vector slice `vec[1]` to `vec[3]`.
+    // | slice: {                                 |
+    // |   ptr: 0x60001238,                       | 8 bytes (pointer to `vec[1]` on heap)
+    // |   len: 3                                 | 8 bytes
+    // | }                                        |
+    // +------------------------------------------+ 0x7ffeefbff4c8 <--- reference to entire vector as a slice `vec[0]` to `vec[4]
+    // | wholeslice: {                            |
+    // |   ptr: 0x60001234,                       | 8 bytes (pointer to `vec[0]` on heap)
+    // |   len: 5                                 | 8 bytes
+    // | }                                        |
+    // +------------------------------------------+ 0x7ffeefbff4d8
+    //  HEAP:
+    // +------------------------------------------+ 0x60001234
+    // | 1                                        | <--- vec[0]
+    // +------------------------------------------+ 0x60001238
+    // | 2                                        | <--- vec[1]
+    // +------------------------------------------+ 0x6000123C
+    // | 3                                        | <--- vec[2]
+    // +------------------------------------------+ 0x60001240
+    // | 4                                        | <--- vec[3]
+    // +------------------------------------------+ 0x60001244
+    // | 5                                        | <--- vec[4]
+    // +------------------------------------------+
+
 
 // --------------------------------------------------------------------------------
 // ## STRING SLICES (&str)
+//
 // A string slice (str) is essentially the type [u8], a sequence of UTF-8 encoded characters.
 //  1. The type `str` has an unknown size at compile-time because it is designed to represent strings of arbitrary length.
 //  2. Every variable in Rust must have a known size at compile time. This is true even for variables that refer to data
 //     on the heap, because those variables' values are actually represented by pointers which have a known size.
-//  Hence it's not possible to generically allocate a value of type `str` because its size is not fixed.
+// Hence it's not possible to generically allocate a value of type `str` because its size is not fixed.
 // A string slice reference (&str) provides a way to refer to the actual slice (string data) without needing a specific size.
 fn string_slices(){
   // Create an owner of type String pointing to data "hello" allocated on the heap
@@ -103,7 +120,10 @@ fn string_slices(){
   let slice = &s[0..2];
   let slice = &s[..2];
 }
+
+// --------------------------------------------------------------------------------
 // ### STRING SLICES AS PARAMETERS
+//
 // A parameter of type &str can accept both String references (&String) and string slices (&str).
 // 1. If we choose to provide an argument of type &String, this is the same as a slice &str of the entire string.
 // 2. If we choose to provide an argument of type &str, this could be any slice of the entire string.
@@ -118,7 +138,10 @@ fn get_first_word(s: &mut str) ->  &str {
 
   &s[..]
 }
+
+// --------------------------------------------------------------------------------
 // ### STRING LITERALS AS SLICES
+//
 //  String literals are interpreted directly as a reference to a string slice.
 //  The data of a string slice is hardcoded in the read-only section of the executable binary.
 fn string_literals(){
@@ -139,6 +162,7 @@ fn string_literals(){
 
 // --------------------------------------------------------------------------------
 // ## OTHER SLICES
+//
 fn i32_slices() {
   // Creates a fixed-size array stored on stack.
   let  xs:  [i32; 5] = [1, 2, 3, 4, 5];
@@ -156,23 +180,25 @@ fn i32_slices() {
 
 // --------------------------------------------------------------------------------
 // ## MUTABLE SLICE REFERENCES
+//
 // The rules for mutable slice references are the same as for references.
-//  ~ While a mutable reference is in scope, no new references can be declared, and no existing references (inc. the owner) can be used.
-// This is true even for two mutable slice references that refer to different parts of memory but the same owner variable.
+//  ~ While a mutable reference lives, no other references can live, and the owner cannot be used.
+// This is true even for two mutable slice references that refer to different parts of memory.
 fn mutable_slices() {
-  // Creates a fixed-size array xs stored on stack.
+  // xs is an array on the stack.
   let mut xs:  [i32; 5] = [1, 2, 3, 4, 5];
 
-  // Creates a first mutable reference r1 to a slice that borrows part of the array xs.
-  let r1: &mut [i32] = &mut xs[1 .. 4];
-  println!("First element of the slice: {}", r1[0]);
-  // r1 is now out of scope
+  // r1 is a mutable slice reference that borrows elements 2 to 4.
+  let r1: &mut [i32] = &mut xs[1 .. 4];                // <<-- start of r1's lifetime
+  println!("First element of the slice: {}", r1[0]);   // <<-- end of r1's lifetime
 
-  // Creates a second mutable reference r2 to a slice that borrows the remained of the array xs.
-  let r2 = &mut xs[5 .. ];
-  println!("First element of the slice: {}", r2[0]);
-  // println!("First element of the slice: {}", r1[0]);    <<-- Not allowed, as this would mean r2 was used in the scope of r1.
-  // r2 is now out of scope
+  // r2 is a mutable slice reference that borrows elements 5 onwards.
+  let r2 = &mut xs[5 .. ];                 // <<-- start of r2's lifetime
+  println!("First element of the slice: {}", r2[0]);   // <<-- end of r2's lifetime
 
-  xs = [1,3,4,4,32];    // <<-- Allowed, as r1 and r2 are out of scope.
+  // A mutable reference cannot live at the same time as another reference, so below is not allowed:
+  // println!("First element of the slice: {}", r1[0]); // Error: this would mean r2 was used during the lifetime of r1.
+
+  // Allowed, as this is after r1 and r2's lifetime.
+  xs = [1,3,4,4,32];
 }

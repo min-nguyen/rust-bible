f88116bc58faddca3c8a9109a03d2f1a84f9efd4
min-nguyen <minhnguyen1995@gmail.com>
2024-08-26 15:02:25 +0100

    add rust-project.json

--- rustlings/exercises/03_if/if1.rs
+++ rustlings/exercises/03_if/if1.rs
@@ -1,5 +1,6 @@
 fn bigger(a: i32, b: i32) -> i32 {
     // TODO: Complete this function to return the bigger number!
+    if a > b { a } else { b}
     // If both numbers are equal, any of them can be returned.
     // Do not use:
     // - another function call
